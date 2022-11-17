2641c8413ef75ac1ebcb3be99103f9fc118900e8
Hayolox <akiklii72@gmail.com>
2022-11-17 18:14:03 +0700

    change table publication

--- database/migrations/2022_11_15_133157_create_publications_table.php
+++ database/migrations/2022_11_15_133157_create_publications_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('author');
             $table->string('title');
             $table->string('content');
+            $table->string('image');
             $table->timestamps();
         });
     }
