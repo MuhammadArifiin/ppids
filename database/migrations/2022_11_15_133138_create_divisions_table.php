e01e329e2bb78324e71af23154b4f807edfba15a
Hayolox <akiklii72@gmail.com>
2022-11-17 19:24:44 +0700

    add atribut image in table division

--- database/migrations/2022_11_15_133138_create_divisions_table.php
+++ database/migrations/2022_11_15_133138_create_divisions_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('division');
             $table->string('employee');
+            $table->string('image');
             $table->timestamps();
         });
     }
