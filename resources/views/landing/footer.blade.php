f43bfe9feeadfb538eb1158d2159d8fe0bbab01a
Hayolox <akiklii72@gmail.com>
2022-12-01 07:46:27 +0700

    coment social media

--- resources/views/landing/footer.blade.php
+++ resources/views/landing/footer.blade.php
@@ -6,12 +6,12 @@
                     <h4 class="navbar-brand">PPIDS<span class="dot">.</span></h4>
                     <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
                         classical Latin literature from</p>
-                    <div class="col-auto social-icons">
+                    {{-- <div class="col-auto social-icons">
                         <a href="#"><i class='bx bxl-facebook'></i></a>
                         <a href="#"><i class='bx bxl-twitter'></i></a>
                         <a href="#"><i class='bx bxl-instagram'></i></a>
                         <a href="#"><i class='bx bxl-pinterest'></i></a>
-                    </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
