bc82150b2b0d006b30e940388a0fc4a7f4c93ad0
Hayolox <akiklii72@gmail.com>
2022-12-01 20:24:08 +0700

    add manage feature

--- app/Http/Controllers/publicationsController.php
+++ app/Http/Controllers/publicationsController.php
@@ -38,11 +38,33 @@ class publicationsController extends Controller
     public function getArticle($id)
     {
         $publications = Publications::find($id);
+
+        $managePublication = ManageFeature::where('name_feature', 'publication')->get();
+     
+        foreach($managePublication as $data){
+            $managePublication = $data->active;
+        }
+
+        $manageAbout = ManageFeature::where('name_feature', 'about')->get();
+        foreach($manageAbout as $data){
+            $manageAbout = $data->active;
+        }
+
+        $manageDivision = ManageFeature::where('name_feature', 'division')->get();
+        foreach($manageDivision as $data){
+            $manageDivision = $data->active;
+        }
+
+        $manageFacility = ManageFeature::where('name_feature', 'facility')->get();
+        foreach($manageFacility as $data){
+            $manageFacility = $data->active;
+        }
         $publications->update([
             'tab_content' => $publications->tab_content + 1
         ]);
 
-        return view('landing.article', compact('publications'));
+        return view('landing.article', compact('publications','managePublication',
+        'manageAbout','manageDivision','manageFacility','publications'));
     }
 
     public function search(Request $request)
